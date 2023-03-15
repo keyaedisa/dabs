71b999a002a8ca6f45e5055cee65a5f435e59fa0
keyaedisa <w0rlde4ter@proton.me>
2023-03-15 14:45:36 -0400

    remove include from .gitignore

---
+++ include/dab.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef uint8_t dabRunOpts_t, *dabRunOpts_p;
+extern dabRunOpts_t hostFlags_t;
+extern dabRunOpts_p hostFlags_p;
+
+extern dabRunProperties thisRun;
+
+typedef struct {
+    dabRunOpts_t hostFlagsT;
+    char* customProfile;
+} dabRunProperties;
+
+int customHandler(dabRunProperties *st);
+
+
+#define dabCF  1                //dab custom flag no profile provided
+#define dabCFPP (1 << 1)        //dab custom flag profile provided
+#define dabVF   (1 << 2)        // dab Vanilla flag
+#define dabVRF  (1 << 3)        // dab vanilla releng flag
+#define dabVBF  (1 << 4)        // dab vanilla baseline flag
+#define dabXF   (1 << 5)        // dab xero flag, no DE specified
+#define dabXKF  (1 << 6)        // dab xero kde flag
+#define dabXGF  (1 << 7)        // dab xero gnome flag
+
